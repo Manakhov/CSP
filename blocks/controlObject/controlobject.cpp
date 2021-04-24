35a9ffe3a84c34382b62c8ffc2191b185170a0a3
Manakhov <manakhovsvyatoslav@gmail.com>
2021-04-24 05:32:11 +0300

    optimize syntax

--- blocks/controlObject/controlobject.cpp
+++ blocks/controlObject/controlobject.cpp
@@ -1,27 +1,13 @@
 #include "controlobject.h"
 
 ControlObject::ControlObject(float a11, float a12, float a13, float a21, float a22, float a23, float a31, float a32, float a33,
-                             float b1, float b2, float b3, float c1, float c2, float c3, float d, float x1, float x2, float x3)
+                             float b1, float b2, float b3, float c1, float c2, float c3, float d, float x1, float x2, float x3):
+    m_a11(a11), m_a12(a12), m_a13(a13), m_a21(a21), m_a22(a22), m_a23(a23), m_a31(a31), m_a32(a32), m_a33(a33),
+    m_b1(b1), m_b2(b2), m_b3(b3),m_c1(c1), m_c2(c2), m_c3(c3), m_d(d), m_prev_input(0.0)
 {
     m_integrator_X1 = new integrator(x1);
     m_integrator_X2 = new integrator(x2);
     m_integrator_X3 = new integrator(x3);
-    m_a11 = a11;
-    m_a12 = a12;
-    m_a13 = a13;
-    m_a21 = a21;
-    m_a22 = a22;
-    m_a23 = a23;
-    m_a31 = a31;
-    m_a32 = a32;
-    m_a33 = a33;
-    m_b1 = b1;
-    m_b2 = b2;
-    m_b3 = b3;
-    m_c1 = c1;
-    m_c2 = c2;
-    m_c3 = c3;
-    m_d = d;
 }
 
 ControlObject::~ControlObject()
